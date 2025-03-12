1e30c2a2dbe6d0dbaccbc3b77c00ed6f11c26d84
igbas <igbas1990@yandex.ru>
2025-03-12 20:46:22 +0700

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,39 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(s string) []string {
+	wordsCounter := map[string]int{}
+	var words []string
+
+	for _, v := range strings.Fields(s) {
+		if _, ok := wordsCounter[v]; ok {
+			wordsCounter[v]++
+		} else {
+			words = append(words, v)
+			wordsCounter[v] = 1
+		}
+	}
+
+	if len(words) < 2 {
+		return words
+	}
+
+	sort.Slice(words, func(i, j int) bool {
+		a, b := wordsCounter[words[i]], wordsCounter[words[j]]
+		if a == b {
+			return words[i] < words[j]
+		}
+
+		return a > b
+	})
+
+	if l := len(words); l > 10 {
+		return words[:10]
+	}
+
+	return words
 }
