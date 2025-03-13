a3530502ff5c751ad7b1f4c8eeca0e478fc39b50
igbas <igbas1990@yandex.ru>
2025-03-13 20:27:52 +0700

    HW4 is completed!

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,77 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	front *ListItem
+	back  *ListItem
+	len   int
+}
+
+func (l *list) Len() int {
+	return l.len
+}
+
+func (l *list) Front() *ListItem {
+	return l.front
+}
+
+func (l *list) Back() *ListItem {
+	return l.back
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	newItem := &ListItem{Value: v}
+
+	if l.len == 0 {
+		l.front = newItem
+		l.back = newItem
+	} else {
+		newItem.Next = l.front
+		l.front.Prev = newItem
+		l.front = newItem
+	}
+
+	l.len++
+
+	return newItem
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	newItem := &ListItem{Value: v}
+
+	if l.len == 0 {
+		l.front = newItem
+		l.back = newItem
+	} else {
+		newItem.Prev = l.back
+		l.back.Next = newItem
+		l.back = newItem
+	}
+
+	l.len++
+
+	return newItem
+}
+
+func (l *list) Remove(i *ListItem) {
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+		i.Prev = nil
+	}
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+		i.Next = nil
+	}
+
+	l.len--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if i == l.front {
+		return
+	}
+
+	l.Remove(i)
+	*i = *l.PushFront(i.Value)
 }
 
 func NewList() List {
