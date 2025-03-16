3fd4ab40d52ca9272707bcde2a9b34b30e90f6bf
igbas <igbas1990@yandex.ru>
2025-03-16 12:54:08 +0700

    HW5 refactor step 1

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -3,6 +3,7 @@ package hw05parallelexecution
 import (
 	"errors"
 	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -12,26 +13,9 @@ type Task func() error
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
 	tCh := make(chan Task)
-	var errCount int
+	var errCount int64
 	var isErrorLimitExceeded bool
-	wg, mu := sync.WaitGroup{}, sync.RWMutex{}
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		defer close(tCh)
-		for _, task := range tasks {
-			mu.RLock()
-			if errCount >= m {
-				isErrorLimitExceeded = true
-			}
-			mu.RUnlock()
-			if isErrorLimitExceeded {
-				break
-			}
-			tCh <- task
-		}
-	}()
+	wg := sync.WaitGroup{}
 
 	for i := 0; i < n; i++ {
 		wg.Add(1)
@@ -39,15 +23,26 @@ func Run(tasks []Task, n, m int) error {
 			defer wg.Done()
 			for task := range tCh {
 				if err := task(); err != nil {
-					mu.Lock()
-					errCount++
-					mu.Unlock()
+					atomic.AddInt64(&errCount, 1)
 				}
 			}
 		}()
 	}
 
+	maxErr := int64(m)
+	for _, task := range tasks {
+		if atomic.LoadInt64(&errCount) >= maxErr {
+			isErrorLimitExceeded = true
+		}
+		if isErrorLimitExceeded {
+			break
+		}
+		tCh <- task
+	}
+
+	close(tCh)
 	wg.Wait()
+
 	if isErrorLimitExceeded {
 		return ErrErrorsLimitExceeded
 	}
