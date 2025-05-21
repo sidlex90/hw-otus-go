c82995e5b4ebd41b4580cbc7923465bcf8e5db79
igbas <igbas1990@yandex.ru>
2025-05-21 18:23:28 +0700

    HW7 is completed

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,33 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestCopy(t *testing.T) {
 	// Place your code here.
+	t.Run("valid copy", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "test_copy.txt", 0, 0)
+		defer os.Remove("test_copy.txt")
+		if err != nil {
+			t.Errorf("expected nil, got %v", err)
+		}
+	})
+
+	t.Run("valid copy with limit out of range", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "test_copy.txt", 0, 10000)
+		defer os.Remove("test_copy.txt")
+		if err != nil {
+			t.Errorf("expected nil, got %v", err)
+		}
+	})
+
+	t.Run("invalid offset out of range", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "test_copy.txt", 10000, 0)
+		defer os.Remove("test_copy.txt")
+		if err == nil {
+			t.Errorf("expected error, got nil")
+		}
+	})
 }
