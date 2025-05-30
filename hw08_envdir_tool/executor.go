749113b33744111515672c90ee97ff90fd92536f
igbas <igbas1990@yandex.ru>
2025-05-30 18:10:51 +0700

    HW08 is completed

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,60 @@
 package main
 
+import (
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	if len(cmd) == 0 {
+		return 1 // Return error code if no command is provided
+	}
+
+	var command *exec.Cmd
+	if len(cmd) == 1 {
+		command = exec.Command(cmd[0])
+	} else {
+		command = exec.Command(cmd[0], cmd[1:]...)
+	}
+
+	// Set up environment variables
+	command.Env = os.Environ()
+	for key, value := range env {
+		if value.NeedRemove {
+			command.Env = removeEnvVar(command.Env, key)
+		} else {
+			command.Env = append(command.Env, key+"="+value.Value)
+		}
+	}
+
+	// Set the command's standard input, output, and error to the current process
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	// Run the comman
+	if err := command.Run(); err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			return exitError.ExitCode() // Return the exit code from the command
+		}
+
+		log.Fatal(err)
+		return 1 // Return a generic error code for other errors
+	}
+
+	return 0 // Return 0 if the command runs successfully
+}
+
+func removeEnvVar(env []string, key string) []string {
+	var result []string
+	prefix := key + "="
+	for _, e := range env {
+		if !strings.HasPrefix(e, prefix) {
+			result = append(result, e)
+		}
+	}
+	return result
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	args := os.Args[1:]
+
+	if len(args) < 2 {
+		os.Stderr.WriteString("Usage: envdir <path_to_env> <command> <arg1> <arg2>....\n")
+		os.Exit(1)
+	}
+
+	pathToEnv := args[0]
+	commandData := args[1:]
+
+	env, err := ReadDir(pathToEnv)
+	if err != nil {
+		os.Stderr.WriteString(err.Error() + "\n")
+		os.Exit(1)
+	}
+
+	RunCmd(commandData, env)
+
+	return
 }
